0fd21d601371a1fb891a0ca2971253931b7b3857
Dario Russo <dariorusso987@gmail.com>
2023-12-27 10:05:23 +0100

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem").then((res)=>{
+	return res.json();}).then((items)=>{
+	 
+const eminem = document.querySelector("#eminem div");
+for(const album of albums){
+
+}
+
+});
